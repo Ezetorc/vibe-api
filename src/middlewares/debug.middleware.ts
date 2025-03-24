fd3a6fd8e6e56dec76651bee0729e0f767a3bd22
Ezetorc <ezetorc@gmail.com>
2025-03-24 15:32:45 -0300

    Updated something

--- src/middlewares/debug.middleware.ts
+++ src/middlewares/debug.middleware.ts
@@ -5,9 +5,8 @@ export function debugMiddleware (
   response: Response,
   next: NextFunction
 ) {
-  const requestOrigin = request.headers.origin ?? request.headers.referer
-  
-  console.log('Origin: ', requestOrigin)
+  console.log('Origin: ', request.headers.origin)
+  console.log('Referer: ', request.headers.referer)
   console.log('Request: ', Boolean(request))
   console.log('Response: ', Boolean(response))
   console.log('Request Headers: ', request.headers)
--- src/settings.ts
+++ src/settings.ts
@@ -23,4 +23,3 @@ export const SALT_ROUNDS: number = Number(envData.SALT_ROUNDS) || 10
 export const SECRET_KEY: string = envData.SECRET_KEY || 'default_key'
 export const CLOUDINARY = cloudinary.v2
 export const DATABASE = mysqlConnection
-export const NODE_ENV = envData.NODE_ENV
